8975a49e9fe4163a84cb17ad502e59603d1ba852
Jonathan Mourier <jonathan.mourier@hotmail.com>
2025-03-10 08:59:30 +0100

    add initial configuration files and SVG icons

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+    i18n: {
+        locales: ["fr", "en"],
+        defaultLocale: "fr",
+        fallback: {
+            en: "fr"
+        },
+        prefixDefaultLocale: true,
+        routing: {
+            fallbackType: "rewrite"
+        }
+    },
+    experimental: {
+        svg: true,
+    },
+});
