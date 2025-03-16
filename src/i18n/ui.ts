94ea970317265fe985232cce6de55aff480b7740
Jonathan Mourier <jonathan.mourier@hotmail.com>
2025-03-16 16:08:29 +0100

    ajout de nouvelles images et polices, mise à jour des composants de la page d'accueil et ajout de la prise en charge des traductions

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    fr: {
+      'nav.who-i-am': 'Qui suis-je',
+      'nav.blog': 'Mon blog',
+      'nav.contact': 'Contact',
+    },
+    en: {
+        'nav.who-i-am': 'Who I am',
+        'nav.blog': 'My blog',
+        'nav.contact': 'Contact',
+    },
+  } as const;
